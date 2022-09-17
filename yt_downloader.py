de04fe889fab83b7ff685e9ec840e8584ad94802
castleking1810 <18ereli@gmail.com>
2022-09-17 20:27:47 +0300

    added download for single file

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,63 +1,125 @@
 # Author: Erel Dagan, DevOps Engineer. ereldag18@gmail.com
 
+
 import PySimpleGUI as sg
 from pytube import YouTube
 from pytube import Playlist
 import os
 
+# ----------- download functions -----------
 
-# set theme
-sg.theme("DarkBrown4")
 
-# ----------- Create the 3 layouts this Window will display -----------
+def download_song(song_url, file_name, file_path):
+    try:
+        yt = YouTube(song_url)
+        print(yt.title, file_name, file_path)
+        if file_name and file_path:
+            yt.streams.get_audio_only().download(filename=file_name, output_path=file_path)
+        elif file_name:
+            yt.streams.get_audio_only().download(filename=file_name)
+        elif file_path:
+            yt.streams.get_audio_only().download(output_path=file_path)
+        else:
+            yt.streams.get_audio_only().download()
+        return True
+    except:
+        print('something went wrong')
+        return False
+
+
+def download_single_song(action, values):
+    print(action, values)
+    url = values[f'-IN-{action}-URL-']
+    file_name = values[f'-IN-{action}-name-']
+    file_path = values[f'-IN-{action}-path-']
+
+    if not os.path.exists(file_path):
+        pass
+        # TODO: add validation output
+    if '.' in file_name:
+        file_name = file_name
+
+    download_song(url, file_name, file_path)
+
+
+def download_csv_songs(action, values):
+    pass
+
+
+def download_playlist(action, values):
+    pass
+
+
+# ----------- functions to create the layouts this Window will display -----------
 
 
 def create_single_song_layout(action):
     layout = [[sg.Text(action)],
               [sg.Text("youtube song URL:"), sg.InputText(
-                  key=f'-IN-{action}-')],
+                  key=f'-IN-{action}-URL-')],
+              [sg.Text("file name:"), sg.InputText(
+                  key=f'-IN-{action}-name-')],
+              [sg.Text("download location:"), sg.Text(), sg.FolderBrowse(
+                  key=f'-IN-{action}-path-')],
               [sg.Submit(key=f'-SUB-{action}-')]]
     return sg.Column(layout, key=f'-COL-{action}-', visible=False)
 
 
 def create_csv_layout(action):
     layout = [[sg.Text(action)],
-              [sg.Input(key=f'-IN-{action}-')],
-              [sg.Input(key=f'-IN2-{action}-')]]
+              [sg.Text("csv location:"), sg.Text(), sg.FileBrowse(
+                  key=f'-IN-{action}-file-')],
+              [sg.Submit()]]
     return sg.Column(layout, key=f'-COL-{action}-', visible=False)
 
 
 def create_playlist_layout(action):
     layout = [[sg.Text(action)],
               [sg.Text("youtube playlist URL:"),
-               sg.InputText(key=f'-IN-{action}-')],
+               sg.InputText(key=f'-IN-{action}-url-')],
               [sg.Submit()]]
     return sg.Column(layout, key=f'-COL-{action}-', visible=False)
 
 
-# ----------- Create actual layout using Columns and a row of Buttons
-MENU = {"download single song": create_single_song_layout,
-        "download songs from csv": create_csv_layout, "download playlist": create_playlist_layout}
+def main():
 
-cols = []
-menu_btns = []
-for action, create_col in MENU.items():
-    cols.append(create_col(action))
-    menu_btns.append(sg.Button(action))
+    # set theme
+    sg.theme("DarkBrown4")
 
-window = sg.Window('Swapping the contents of a window', [cols, menu_btns])
+    # ----------- Create layout using specified functions and index
+    MENU = {"download single song": [create_single_song_layout, download_single_song],
+            "download songs from csv": [create_csv_layout, download_csv_songs],
+            "download playlist": [create_playlist_layout, download_playlist]}
+
+    cols = []
+    menu_btns = []
+    for action, func in MENU.items():
+        cols.append(func[0](action))
+        menu_btns.append(sg.Button(action))
+
+    # create window
+    window = sg.Window('youtube downloader', [cols, menu_btns])
+
+    # event loop
+    current_action = ''  # The currently visible layout
+    while True:
+        event, values = window.read()
+        print('\n---------------------------\n')
+        print(event, values)
+        if event in (None, 'Exit'):
+            break
+
+        # switch layout upon button press
+        if event in MENU.keys():
+            if current_action != '':
+                window[f'-COL-{current_action}-'].update(visible=False)
+            current_action = event
+            window[f'-COL-{current_action}-'].update(visible=True)
+        elif event.find('-SUB-') != -1:
+
+            MENU[current_action][1](current_action, values)
+    window.close()
 
 
-# event loop
-cur_layout = ''  # The currently visible layout
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event in (None, 'Exit'):
-        break
-    if event in MENU.keys():
-        if cur_layout != '':
-            window[f'-COL-{cur_layout}-'].update(visible=False)
-        cur_layout = event
-        window[f'-COL-{cur_layout}-'].update(visible=True)
-window.close()
+if __name__ == '__main__':
+    main()
