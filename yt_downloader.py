32f3deb02c88cad712305357cdc3823d3e7067ac
castleking1810 <18ereli@gmail.com>
2022-09-17 17:27:25 +0300

    changed keys and layouts

--- yt_downloader.py
+++ yt_downloader.py
@@ -5,28 +5,36 @@ from pytube import YouTube
 from pytube import Playlist
 import os
 
+
+# set theme
+sg.theme("DarkBrown4")
+
 # ----------- Create the 3 layouts this Window will display -----------
 single_file_layout = [[sg.Text('download single song')],
-                      *[[sg.CB(f'Checkbox {i}')] for i in range(5)]]
+                      [sg.Text("youtube song URL:"), sg.InputText(
+                          key='-IN-single-song-url')],
+                      [sg.Submit(key='-SUB-download single song-')]]
 
 multiple_from_csv_layout = [[sg.Text('download all songs from csv')],
                             [sg.Input(key='-IN-')],
                             [sg.Input(key='-IN2-')]]
 
 playlist_layout = [[sg.Text('download all songs in playlist')],
-                   *[[sg.R(f'Radio {i}', 1)] for i in range(8)]]
+                   [sg.Text("youtube playlist URL:"),
+                    sg.InputText(key='-IN-playlist-url')],
+                   [sg.Submit()]]
 
 # ----------- Create actual layout using Columns and a row of Buttons
-MENU_ACTIONS = {"download single song": single_file_layout,
-                "download songs from csv": multiple_from_csv_layout, "download playlist": playlist_layout}
+MENU = {"download single song": single_file_layout,
+        "download songs from csv": multiple_from_csv_layout, "download playlist": playlist_layout}
 
 cols = []
-menu = []
-for action, layout in MENU_ACTIONS.items():
-    cols.append(sg.Column(layout, key=f'-col-{action}', visible=False))
-    menu.append(sg.Button(action))
+menu_btns = []
+for action, layout in MENU.items():
+    cols.append(sg.Column(layout, key=f'-COL-{action}-', visible=False))
+    menu_btns.append(sg.Button(action))
 
-window = sg.Window('Swapping the contents of a window', [cols, menu])
+window = sg.Window('Swapping the contents of a window', [cols, menu_btns])
 
 
 # event loop
@@ -36,8 +44,9 @@ while True:
     print(event, values)
     if event in (None, 'Exit'):
         break
-    if event in MENU_ACTIONS.keys():
-        window[f'-COL{cur_layout}-'].update(visible=False)
-        cur_layout = int(event)
-        window[f'-COL{cur_layout}-'].update(visible=True)
+    if event in MENU.keys():
+        if cur_layout != '':
+            window[f'-COL-{cur_layout}-'].update(visible=False)
+        cur_layout = event
+        window[f'-COL-{cur_layout}-'].update(visible=True)
 window.close()
