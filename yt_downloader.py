3b70604bd7fda8c8750179eb316707a23130e00e
castleking1810 <18ereli@gmail.com>
2022-09-17 16:35:40 +0300

    changed from tkinter to pysimplegui

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,53 +1,43 @@
 # Author: Erel Dagan, DevOps Engineer. ereldag18@gmail.com
 
+import PySimpleGUI as sg
 from pytube import YouTube
 from pytube import Playlist
-import tkinter as tk
 import os
 
-BG_COLOR = "#181818"
-FG_COLOR = "#A80000"
-COL_SIZE = 5
-ROW_SIZE = 5
+# ----------- Create the 3 layouts this Window will display -----------
+single_file_layout = [[sg.Text('download single song')],
+                      *[[sg.CB(f'Checkbox {i}')] for i in range(5)]]
+
+multiple_from_csv_layout = [[sg.Text('download all songs from csv')],
+                            [sg.Input(key='-IN-')],
+                            [sg.Input(key='-IN2-')]]
+
+playlist_layout = [[sg.Text('download all songs in playlist')],
+                   *[[sg.R(f'Radio {i}', 1)] for i in range(8)]]
+
+# ----------- Create actual layout using Columns and a row of Buttons
+MENU_ACTIONS = {"download single song": single_file_layout,
+                "download songs from csv": multiple_from_csv_layout, "download playlist": playlist_layout}
+
+cols = []
 menu = []
-root = tk.Tk()
+for action, layout in MENU_ACTIONS.items():
+    cols.append(sg.Column(layout, key=f'-col-{action}', visible=False))
+    menu.append(sg.Button(action))
+
+window = sg.Window('Swapping the contents of a window', [cols, menu])
 
 
-def init_root():
-    root.title("Youtube downloader and cd burner")
-    root.configure(bg=BG_COLOR)
-    for i in range(COL_SIZE):
-        root.columnconfigure(i, weight=1, minsize=75)
-        for j in range(ROW_SIZE):
-            root.rowconfigure(j, weight=1, minsize=50)
-
-    create_menu()
-    return root
-
-
-def create_menu():
-    # welcome_label = tk.Label(text="welcome!", font=(
-    #    "Arial", "25"), fg=FG_COLOR, bg=BG_COLOR)
-    #welcome_label.grid(column=2, row=0)
-    title_label = tk.Label(master=root, text="YouTube downloader", font=(
-        "Arial", "25"), fg=FG_COLOR, bg=BG_COLOR)
-    title_label.grid(column=0, row=0)
-
-    menu_frame = tk.Frame(master=root,width='100%',height='100%')
-    menu_frame.grid(column=0, row=1, rowspan=4)
-    global menu
-    for i in range(1, 5):
-        btn = tk.Button(master=menu_frame, text="click", fg=FG_COLOR,
-                        command=lambda: print("clicked"))
-        btn.grid(row=i, column=0)
-        menu.append(btn)
-    #menu[0].configure(text="")
-
-
-def main():
-    root = init_root()
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+# event loop
+cur_layout = ''  # The currently visible layout
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event in (None, 'Exit'):
+        break
+    if event in MENU_ACTIONS.keys():
+        window[f'-COL{cur_layout}-'].update(visible=False)
+        cur_layout = int(event)
+        window[f'-COL{cur_layout}-'].update(visible=True)
+window.close()
