ea43cd151a53bc9fa5d16c6fde24f6e5bad942d5
castleking1810 <18ereli@gmail.com>
2022-09-19 18:36:31 +0300

    added download from csv, changes to program output

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,6 +1,6 @@
 # Author: Erel Dagan, DevOps Engineer. ereldag18@gmail.com
 
-
+from csv import reader
 import PySimpleGUI as sg
 from pytube import YouTube
 from pytube import Playlist
@@ -9,22 +9,30 @@ import os
 # ----------- download functions -----------
 
 
-def download_song(song_url, file_name, file_path):
-    try:
-        yt = YouTube(song_url)
-        print(yt.title, file_name, file_path)
-        if file_name and file_path:
-            yt.streams.get_audio_only().download(filename=file_name, output_path=file_path)
-        elif file_name:
-            yt.streams.get_audio_only().download(filename=file_name)
-        elif file_path:
-            yt.streams.get_audio_only().download(output_path=file_path)
-        else:
-            yt.streams.get_audio_only().download()
-        return True
-    except Exception as e:
-        print(e.message)
-        return False
+def download_song(url, file_name, file_path, output):
+    if '.' not in file_name:
+        file_name = file_name + ".mp3"
+    if not os.path.exists(file_path):
+        output.update(output.get()+'\n'+'failed - path not found!')
+        # TODO: add validation output
+    else:
+        try:
+            yt = YouTube(url)
+            output.update(
+                output.get()+'\n'+f'downloading {yt.title} as {file_path}\\{file_name} ')
+            print(yt.title, file_name, file_path)
+            if file_name and file_path:
+                yt.streams.get_audio_only().download(filename=file_name, output_path=file_path)
+            elif file_name:
+                yt.streams.get_audio_only().download(filename=file_name)
+            elif file_path:
+                yt.streams.get_audio_only().download(output_path=file_path)
+            else:
+                yt.streams.get_audio_only().download()
+            output.update(output.get()+'\n'+'success!')
+        except Exception as e:
+            output.update(output.get()+'\n'+'failed... view error message below')
+            output.update(output.get()+'\n'+f'{e.args}')
 
 
 def download_single_song(window, action, values):
@@ -32,23 +40,24 @@ def download_single_song(window, action, values):
     url = values[f'-IN-{action}-URL-']
     file_name = values[f'-IN-{action}-name-']
     file_path = values[f'-IN-{action}-path-']
-
-    if not os.path.exists(file_path):
-        pass
-        # TODO: add validation output
-    if '.' in file_name:
-        file_name = file_name
-
     output = window[f'-OUT-{action}-']
-    status = download_song(url, file_name, file_path)
-    if status:
-        output.update(output.get()+'success!\n')
-    else:
-        output.update(output.get()+'failed...\n')
+
+    download_song(url, file_name, file_path, output)
 
 
 def download_csv_songs(window, action, values):
-    pass
+    csv_path = values[f'-IN-{action}-file-']
+    output = window[f'-OUT-{action}-']
+
+    output.update(output.get()+'\n'+f'reading csv {csv_path}...')
+    with open(csv_path, newline='') as csv:
+        for line in reader(csv):
+            file_name = line[0].replace('\t', '')
+            file_path = line[1]
+            url = line[2]
+            download_song(url, file_name, file_path, output)
+
+    output.update(output.get()+'\n'+f'finished reading {csv_path}')
 
 
 def download_playlist(window, action, values):
@@ -68,15 +77,15 @@ def create_single_song_layout(action):
                   key=f'-IN-{action}-path-')],
               [sg.Submit(key=f'-SUB-{action}-')],
               [sg.ProgressBar(key=f'-PROG-{action}-', max_value=20)],
-              [sg.Multiline(key=f'-OUT-{action}-',)]]
+              [sg.Multiline(key=f'-OUT-{action}-', size=(50, 10))]]
     return sg.Column(layout, key=f'-COL-{action}-', visible=False)
 
 
 def create_csv_layout(action):
     layout = [[sg.Text(action)],
               [sg.Text("csv location:"), sg.Text(), sg.FileBrowse(
-                  key=f'-IN-{action}-file-')],
-              [sg.Submit()],
+                  key=f'-IN-{action}-file-', file_types=(("comma seperated values", "*.csv"),),)],
+              [sg.Submit(key=f'-SUB-{action}-')],
               [sg.ProgressBar(key=f'-PROG-{action}-', max_value=20)],
               [sg.Multiline(key=f'-OUT-{action}-',)]]
     return sg.Column(layout, key=f'-COL-{action}-', visible=False)
@@ -86,7 +95,7 @@ def create_playlist_layout(action):
     layout = [[sg.Text(action)],
               [sg.Text("youtube playlist URL:"),
                sg.InputText(key=f'-IN-{action}-url-')],
-              [sg.Submit()],
+              [sg.Submit(key=f'-SUB-{action}-')],
               [sg.ProgressBar(key=f'-PROG-{action}-', max_value=20)],
               [sg.Multiline(key=f'-OUT-{action}-',)]]
     return sg.Column(layout, key=f'-COL-{action}-', visible=False)
@@ -133,6 +142,7 @@ def main():
         elif event.find('-SUB-') != -1:
 
             MENU[current_action][1](window, current_action, values)
+
 
     window.close()
 
